fae7132b1b4fed1d2fe8e226364f0da5cb5c778f
Joshua Thompson <ysovuka@gmail.com>
2017-08-27 21:47:39 -0500

    Knockout + DataTables

---
+++ src/AspNetCore.Knockout.DataTables.Web/Controllers/ApiController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCore.Knockout.DataTables.Web.Models;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCore.Knockout.DataTables.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            return JsonConvert.SerializeObject(new List<CustomFieldModel>
+                {
+                    new CustomFieldModel
+                    {
+                        Id = 1,
+                        Name = "FirstName",
+                        Value = "Joshua",
+                    },
+                    new CustomFieldModel
+                    {
+                        Id = 2,
+                        Name = "LastName",
+                        Value = "Thompson",
+                    }
+            });
+        }
+    }
+}
